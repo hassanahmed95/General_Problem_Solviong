20eca8f06d28c08ae4ff1ee4fbc0bbf3ae3909d2
hassanahmed95 <mr.hassanahmed95@gmail.com>
2021-07-27 15:23:33 +0500

    find missing number in given array

--- problem1.py
+++ problem1.py
@@ -2,7 +2,7 @@
 # given integer array of 1 to 100?
 def find_missing_number():
     data = [1, 2, 4, 5, 6]
-    n = 6
+    n = 6 
     total_sum = n * (n + 1) / 2
     res = total_sum - sum(data)
     return int(res)
