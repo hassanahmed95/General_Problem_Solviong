b6d43533a4f95aba5e7fd678107ae28ed58dabee
hassanahmed95 <mr.hassanahmed95@gmail.com>
2021-07-27 18:02:48 +0500

    find all duplicate numbers

--- problem3.py
+++ problem3.py
@@ -1,17 +1,15 @@
 # find duplicate numbers from a given integer array
 # solving problem using brute force approach
 def find_duplicate_number(data):
-    data = [3, 4, 1, 4, 1]
     test = []
-
     for i in range(0, len(data)):
         for j in range(i + 1, len(data)):
-            # print(data[j])
             if data[i] == data[j]:
                 test.append(data[i])
     return list(set(test))
 
 
 if __name__ == "__main__":
-    result = find_duplicate_number()
+    testing_data = [3, 4, 1, 4, 1, 1]
+    result = find_duplicate_number(testing_data)
     print(result)
