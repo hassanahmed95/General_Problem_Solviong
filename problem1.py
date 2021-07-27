c1356c38f50ffc40f29aa677416feba534f3bb2f
hassanahmed95 <mr.hassanahmed95@gmail.com>
2021-07-27 15:45:03 +0500

    find missing number from given array

--- problem1.py
+++ problem1.py
@@ -1,13 +1,13 @@
 # How do you find the missing number in a
 # given integer array of 1 to 100?
-def find_missing_number():
-    data = [1, 2, 4, 5, 6]
-    n = 6 
+def find_missing_number(data):
+    n = 6
     total_sum = n * (n + 1) / 2
     res = total_sum - sum(data)
     return int(res)
 
 
 if __name__ == "__main__":
-    result = find_missing_number()
+    test_data = [1, 2, 4, 5, 6]
+    result = find_missing_number(test_data)
     print(result)
--- problem3.py
+++ problem3.py
@@ -1,20 +1,17 @@
-# find duplicate numbers from a given integer array?
-def find_duplicate_number():
-    data = [1, 2, 4, 4, 6, 6, 5]
+# find duplicate numbers from a given integer array
+# solving problem using brute force approach
+def find_duplicate_number(data):
+    data = [3, 4, 1, 4, 1]
     test = []
 
     for i in range(0, len(data)):
-        # print(data[i])
-        for j in range(1, len(data)):
-            print(data[j])
-            if i == j:
-                # if i not in test:
+        for j in range(i + 1, len(data)):
+            # print(data[j])
+            if data[i] == data[j]:
                 test.append(data[i])
-    return test
+    return list(set(test))
 
 
-find_duplicate_number()
-# print(res)
-# if __name__ == "__main__":
-#     result = find_duplicate_number()
-#     print(result)
+if __name__ == "__main__":
+    result = find_duplicate_number()
+    print(result)
